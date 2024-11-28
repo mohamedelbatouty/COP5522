8a2292b49c8cb7d101f00c6310cbae444482a99d
mohamedelbatouty <55361505+mohamedelbatouty@users.noreply.github.com>
2024-11-28 16:01:34 -0500

    Tyler MPI changes

--- mpi.c
+++ mpi.c
@@ -1,9 +1,10 @@
-#include <mpi.h>
+#include <microtime.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <microtime.h>
+#include <mpi.h>
+
 
 void merge(int arr[], int left, int mid, int right) {
     int i, j, k;
@@ -57,65 +58,13 @@ void mergeSort(int arr[], int left, int right) {
     }
 }
 
-void PMerge(int* arr, int left, int right, int rank, int size) {
-    int n = right - left + 1;
-    int chunk = n / size;
-    int rem = n % size;
-    int local_n = chunk + (rank < rem ? 1 : 0);
-    int *local_arr = (int*)malloc(local_n * sizeof(int));
-    int *sizes = (int*)malloc(size * sizeof(int));
-    int *displs = (int*)malloc(size * sizeof(int));
-    int displ = 0;
-    for (int i = 0; i < size; i++) {
-        sizes[i] = chunk + (i < rem ? 1 : 0);
-        displs[i] = displ;
-        displ += sizes[i];
-    }
-
-    MPI_Scatterv(arr + left, sizes, displs, MPI_INT, local_arr, local_n, MPI_INT, 0, MPI_COMM_WORLD);
-    mergeSort(local_arr, 0, local_n - 1);
-    int *sorted_arr = (int*)malloc(n * sizeof(int));
-    MPI_Allgatherv(local_arr, local_n, MPI_INT, sorted_arr, sizes, displs, MPI_INT, MPI_COMM_WORLD);
-    if (rank == 0) {
-        int* temp_arr = (int*)malloc(n * sizeof(int));
-        int start1 = 0;
-        int size1 = sizes[0];
-
-        for (int i = 1; i < size; i++) {
-            int start2 = displs[i];
-            int size2 = sizes[i];
-            int k = 0, left = start1, right = start2;
-            while (left < start1 + size1 && right < start2 + size2) {
-                if (sorted_arr[left] <= sorted_arr[right]) {
-                    temp_arr[k++] = sorted_arr[left++];
-                } else {
-                    temp_arr[k++] = sorted_arr[right++];
-                }
-            }
-            while (left < start1 + size1) {
-                temp_arr[k++] = sorted_arr[left++];
-            }
-            while (right < start2 + size2) {
-                temp_arr[k++] = sorted_arr[right++];
-            }
-            memcpy(sorted_arr + start1, temp_arr, (size1 + size2) * sizeof(int));
-            size1 += size2;
-        }
-        memcpy(arr, sorted_arr, n * sizeof(int));
-        free(temp_arr);
-    }
-    free(local_arr);
-    free(sizes);
-    free(displs);
-    free(sorted_arr);
-}
-
 int* load_test_data(const char* filename, int* out_length) {
     FILE* file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error opening test data file");
         exit(0);
     }
+
     int length;
     if (fscanf(file, "%d", &length) != 1) {
         printf("Test data file must contain array length on line 0");
@@ -124,6 +73,7 @@ int* load_test_data(const char* filename, int* out_length) {
     }
 
     int* arr = (int*)malloc(length * sizeof(int));
+
     for (int i = 0; i < length; i++) {
         if (fscanf(file, "%d", &arr[i]) != 1) {
             printf("Error reading number on line %i", i);
@@ -132,40 +82,102 @@ int* load_test_data(const char* filename, int* out_length) {
             exit(0);
         }
     }
+
+    // Close the file
     fclose(file);
+
+    // Set the output length
     *out_length = length;
+
     return arr;
 }
 
-int main(int argc, char** argv) {
-    int rank, size;
+int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
+    double start_time;
+    
+    int procs, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int loaded_length;
+
     int* loaded_array = NULL;
+    int loaded_length = 0;
 
     if (rank == 0) {
         loaded_array = load_test_data("testdata.txt", &loaded_length);
     }
+
     MPI_Bcast(&loaded_length, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    if (rank != 0)
-        loaded_array = (int*)malloc(loaded_length * sizeof(int));
-    
-    double time1 = microtime();
-    PMerge(loaded_array, 0, loaded_length - 1, rank, size);
-    double time2 = microtime();
+    int* local_array = (int*)malloc(loaded_length * sizeof(int));
+    int local_size = loaded_length / procs;
+    int remainder = loaded_length % procs;
+
+    int* sendcounts = (int*)malloc(procs * sizeof(int));
+    int* displacement = (int*)malloc(procs * sizeof(int));
+
+    for (int i = 0; i < procs; i++) {
+        sendcounts[i] = local_size + (i < remainder ? 1 : 0);
+        displacement[i] = (i > 0) ? (displacement[i-1] + sendcounts[i-1]) : 0;
+    }
+
+    MPI_Scatterv(
+        loaded_array,
+        sendcounts,
+        displacement,
+        MPI_INT,
+        local_array,
+        sendcounts[rank],
+        MPI_INT,
+        0,
+        MPI_COMM_WORLD
+    );
+    if (rank==0){
+    start_time = microtime();
+    }
+    mergeSort(local_array, 0, sendcounts[rank] - 1);
+
+    int* gathered_array = NULL;
+    if (rank == 0) {
+        gathered_array = (int*)malloc(loaded_length * sizeof(int));
+    }
+
+    MPI_Gatherv(
+        local_array,        // send buffer
+        sendcounts[rank],   // send count
+        MPI_INT,            // send type
+        gathered_array,     // receive buffer
+        sendcounts,         // receive counts
+        displacement,             // displacements
+        MPI_INT,            // receive type
+        0,                  // root
+        MPI_COMM_WORLD      // communicator
+    );
 
     if (rank == 0) {
-        printf("\nTime = %g seconds\n", time2 - time1);
+
+        mergeSort(gathered_array, 0, loaded_length - 1);
+        double end_time = microtime();
+
         for (int i = 0; i < loaded_length - 1; i++) {
-            if (loaded_array[i] > loaded_array[i + 1]) {
-                printf("Test data arrays not sorted: %i > %i\n", loaded_array[i], loaded_array[i + 1]);
+            if (gathered_array[i] > gathered_array[i+1]) {
+                printf("Test data arrays not sorted: %i > %i\n",
+                       gathered_array[i], gathered_array[i+1]);
+                break;
             }
         }
+
+        printf("\nExecution Time = %g seconds\n", end_time - start_time);
+
+        free(gathered_array);
         free(loaded_array);
     }
+
+    free(local_array);
+    free(sendcounts);
+    free(displacement);
+
     MPI_Finalize();
+
     return 0;
 }
